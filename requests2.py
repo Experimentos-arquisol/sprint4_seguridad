0faed03a296acc75a1fa89000a7d2180dd706841
RuizL25 <lf.ruizo1@uniandes.edu.co>
2024-05-11 09:28:36 -0500

    ahora si el final

--- requests2.py
+++ requests2.py
@@ -4,10 +4,9 @@ from concurrent.futures import ThreadPoolExecutor
 import datetime
 import time
 
-# Lista para guardar los datos de las peticiones
+
 peticiones = []
 
-# Tiempo inicial global
 tiempo_inicial = time.time()
 
 def enviar_peticion(n):
@@ -16,15 +15,13 @@ def enviar_peticion(n):
     response = requests.post("http://34.30.171.140:8000", json={"correo": "1correo@prueba"}, headers=headers)
     # response = requests.post("http://35.188.104.109:8080/cliente/registro/", json={"correo": "1correo@prueba"}, headers=headers)
 
-    # Captura el momento en que se envió la petición como segundos desde el tiempo inicial
     momento = time.time() - tiempo_inicial
 
-    # Guardar información relevante en la lista
     peticiones.append({
         "Numero de Peticion": n,
         "Estado": response.status_code,
         "Headers": headers["api-key"],
-        "Momento (s)": round(momento, 2)  # Momento en segundos con dos decimales
+        "Momento (s)": round(momento, 2)
     })
     
     print(f"{response.status_code} Petición {n}")
@@ -33,25 +30,21 @@ def main():
     numero_peticiones = 500
 
     global tiempo_inicial
-    tiempo_inicial = time.time()  # Reiniciar el tiempo inicial
+    tiempo_inicial = time.time() 
 
     with ThreadPoolExecutor() as executor:
         futures = [executor.submit(enviar_peticion, i) for i in range(numero_peticiones)]
 
-    # Espera a que todas las peticiones se completen
     for future in futures:
         future.result()
 
-    # Crear un DataFrame con los resultados
     df = pd.DataFrame(peticiones)
 
-    # Contar peticiones rechazadas
     cantidad_malas = len(df[df['Estado'] == 429])
     
     print(f'Peticiones rechazadas: {cantidad_malas}')
     print(f'# de peticiones: {numero_peticiones}')
     
-    # Guardar el DataFrame en un archivo Excel
     df.to_excel("resultados_peticiones.xlsx", index=False)
 
 main()
